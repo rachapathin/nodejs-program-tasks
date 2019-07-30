05733be96d398253e31f3bc3cf3db36a4e53eccf
nageswararao_rachapathi@epam.com <Nageswararao_Rachapathi@epam.com>
2019-07-30 23:19:42 +0530

    Resolved code review comments

--- http-servers/html-server.js
+++ http-servers/html-server.js
@@ -1,10 +1,15 @@
 const http = require('http');
 const fs = require('fs');
+const path = require('path');
+const replace = require('stream-replace');
 
 
 const server =  http.createServer((request, response) => {
     response.writeHead(200, {'Content-Type': 'text/html'});
-    fs.createReadStream(`${process.cwd()}/http-servers/index.html`).pipe(response);
+    const filePath = path.join(__dirname, '/index.html');
+    fs.createReadStream(filePath)
+    .pipe(replace('message', 'New Message'))
+    .pipe(response);
 });
 
 server.listen(7000);
