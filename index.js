ab42c7ec170ab64b290297ed730e08eba702d54d
nageswararao_rachapathi@epam.com <Nageswararao_Rachapathi@epam.com>
2019-08-19 22:02:04 +0530

    As per the evaluation criteria completed node-home-work-6

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 import app from './new-app' ;
+import Sequelize from 'sequelize';
+import dbConfig from './config/database.json';
+
 require('dotenv').config();
 
 app.listen(process.env.PORT, () =>
@@ -8,3 +11,19 @@ app.listen(process.env.PORT, () =>
 app.get('/', (req, res) =>
     res.send( `App listening on port ${process.env.PORT} !` )
 );
+
+const { dialect, username, password, host, database, } = dbConfig.development;
+
+export const sequelize = new Sequelize(database, username, password, {
+    host: host,
+    dialect: dialect
+});
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Database Connected successfully.');
+  })
+  .catch((err) => {
+    console.error('Unable to connect to the database:', err);
+  });
